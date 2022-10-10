7c050ccb871a0bd8c1cd05c45b47388df7771ed6
s-121-23 <s-121-23@gumiforest.ae.krakow.pl>
2022-10-10 11:22:06 +0200

    lekcja2

---
+++ 02-TypesAndVariables/zmienne.py
@@ -0,0 +1,17 @@
+n1=5
+n2=1
+n3=8
+n4=6
+n5=3
+
+suma=(n1+n2+n3+n4+n5)
+print(suma)
+
+kwadraty=(n1**n1+n2**n2+n3**n3+n4**n4+n5**n5)
+print(kwadraty)
+
+quotient=n3/n5
+print(quotient)
+
+rownosc=n3==n4
+print(rownosc)
